728bb273426a5e6ad3f3370243e18a9efffeee5d
Alexander Hoyo <37727476+ahmdev@users.noreply.github.com>
2024-03-19 03:33:36 -0400

    Create database.php

---
+++ database.php
@@ -0,0 +1,8 @@
+<?php
+
+$host = "bdrvqpuk1kiofpy6mbba-mysql.services.clever-cloud.com";
+$usuario = "uyzelyox9slytozt";
+$contrasena = "xR6AKI4asgxtVx4oSfTw";
+$base_datos = "bdrvqpuk1kiofpy6mbba";
+
+?>
